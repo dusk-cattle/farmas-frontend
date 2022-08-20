6807472895f0caba854d8e80147d0e8e4b7c11fb
Lucas Santos Oliveira Rezende <lucas.rezende@kinvo.com.br>
2022-08-20 14:40:13 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+// deps
+import React from 'react';
+
+// screens
+import { Home } from './src/screens';
+
+export default function App() {
+  return <Home />;
+}
---
+++ template.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  placeholderName: 'farmas',
+  templateDir: './template',
+}
