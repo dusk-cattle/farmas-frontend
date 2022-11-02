310253fa3836be94cfc9dad4c59c02aaf3b21317
Lucas Santos Oliveira Rezende <lucas.rezende@kinvo.com.br>
2022-11-02 17:07:27 -0300

    fixes get-coordinates

--- src/backend/controllers/Farm/coordinates.ts
+++ src/backend/controllers/Farm/coordinates.ts
@@ -1,24 +1,27 @@
 // types
-import axios from "axios";
-import { Connections } from "../../enums/connections";
+import axios from 'axios';
+import { Connections } from '../../enums/connections';
 
-import { Point } from "./types";
+import { Point } from './types';
 
 export async function GetCoordinates() {
-   try {
-      const item = localStorage.getItem("user");
-      var token = " ";
-      if (item != null) {
-         token = JSON.parse(item)["token"];
-      }
-      const config = {
-         headers: {
-            Authorization: "Bearer " + token,
-         },
-      };
-      const response = await axios.get<Point>(Connections.FARMAS + "/Size", config);
-      return response.data;
-   } catch (error) {
-      throw new Error("Error while retrieving Farm coordinates");
-   }
+  try {
+    const item = localStorage.getItem('user');
+    var token = ' ';
+    if (item != null) {
+      token = JSON.parse(item)['token'];
+    }
+    const config = {
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    };
+    const response = await axios.get<Point>(
+      Connections.FARMAS + '/farm/Size',
+      config
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error('Error while retrieving Farm coordinates');
+  }
 }
