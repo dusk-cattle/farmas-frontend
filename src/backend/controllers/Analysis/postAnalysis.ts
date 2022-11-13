2b4b69135911615aabe4d8523d45921273224021
Lucas Souza <souzacontato18@gmail.com>
2022-11-13 19:57:39 -0300

    refactor de erros e localstorage

--- src/backend/controllers/Analysis/postAnalysis.ts
+++ src/backend/controllers/Analysis/postAnalysis.ts
@@ -2,38 +2,24 @@ import axios from "axios";
 import { Analysis } from "./types";
 import { LocalData } from "../../enums/localData";
 import { Connections } from "../../enums/connections";
+import { GetUserTokenFromStorage } from "../../utils";
 
 export async function PostAnalysis(analysis: Analysis, isOnline: Boolean) {
-  try {
-    if (isOnline) {
-      const item = localStorage.getItem("user");
-
-      var token = " ";
-
-      if (item != null) {
-        token = JSON.parse(item)["token"];
+   try {
+      if (isOnline) {
+         const token = GetUserTokenFromStorage();
+         const config = {
+            headers: {
+               Authorization: "Bearer " + token,
+            },
+         };
+         return true;
+      } else {
+         localStorage.setItem(LocalData.ANALYSIS_KEY, JSON.stringify(analysis));
+         return true;
       }
-
-      const config = {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      };
-
-      console.log(analysis);
-
-      const response = await axios.post(
-        Connections.FARMAS + "/SoilAnalysis",
-        analysis,
-        config
-      );
-      return true;
-    } else {
-      localStorage.setItem(LocalData.ANALYSIS_KEY, JSON.stringify(analysis));
-      return true;
-    }
-  } catch (error) {
-    console.log(error);
-    throw new Error("Error while trying to submit analysis");
-  }
+   } catch (error) {
+      console.log(error);
+      throw new Error("Erro ao criar Análise");
+   }
 }
